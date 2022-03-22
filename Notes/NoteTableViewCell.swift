b3db08e94887f3b8e0227d3671bc3adc5146a96b
zxbuzzy <zxbuzzy@gmail.com>
2022-03-22 20:44:18 +0300

    create basic UI

---
+++ Notes/NoteTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  NoteTableViewCell.swift
+//  Notes
+//
+//  Created by Никита Андриянников on 22.03.2022.
+//
+
+import UIKit
+
+class NoteTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
